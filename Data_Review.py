87e5d667c47850c672acb7facf748cc38ee80ac4
fatihgulsen <fatihgulsen@outlook.com.tr>
2021-12-10 02:52:06 +0300

    Data Review new dataset

--- Data_Review.py
+++ Data_Review.py
@@ -1,3 +1,4 @@
+# %%
 import time
 import numpy as np
 import pandas as pd
@@ -11,8 +12,43 @@ import matplotlib.pyplot as plt
 warnings.filterwarnings('ignore')
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 500)
+plt.rcParams["figure.figsize"] = [16.00, 16.00]
+plt.rcParams["figure.autolayout"] = True
+sns.set(rc={'figure.figsize': [16.00, 16.00]})
+
+
+def bar_plot(df_catplot, country: str, value: int, _x, _y, _col):
+    chart = sns.catplot(x=_x, y=_y, col=_col,
+                        data=df_catplot.loc[
+                            ((df_catplot[_col] == country) & (df_catplot[_y] > value)
+                             )], kind='bar', legend=True, legend_out=True)
+    chart.set_xticklabels(rotation=90, horizontalalignment='center', fontsize=7)
+    plt.tight_layout()
+    plt.show()
+
+
+def pie_plot(_all_data, _x, _y, _title):
+    colors = sns.color_palette('pastel')
+    _all_data = _all_data.groupby([_x]).sum()
+    _all_data.plot(kind='pie', y=_y, x=_x, autopct='%1.2f%%', colors=colors,
+                   startangle=270, fontsize=17, shadow=True, explode=(0.1, 0.1, 0.1, 0.1, 0.1, 0.1),
+                   legend=False,
+                   title=_title)
+    plt.tight_layout()
+    plt.show()
+
+
+def df_group_agg(df, group: list, agg: dict):
+    new_df = df.groupby(group, as_index=False).agg(agg)
+    cols = new_df.columns
+    new_col = [col[0] if col[1] == '' else col[0] + '_' + col[1] for col in cols]
+    new_df.columns = new_col
+    return new_df
+
+
+# %%
 ##################################################
-# Veri Seti
+# Veri Seti import-exported-value
 ##################################################
 all_data = pd.read_csv(r"VeriSetleri/All_Data/import-exported-value.csv", sep='\t')
 all_data.replace({'0': np.nan, 0: np.nan}, inplace=True)
@@ -25,7 +61,6 @@ check_df(all_data)
 # profile.to_file('profile.html')
 
 # Corr
-plt.rcParams["figure.figsize"] = (20, 15)
 sns.heatmap(all_data.corr(), annot=True)
 plt.show()
 
@@ -58,39 +93,137 @@ df['Importers'].unique()
 df.groupby(["Exporter"]).agg({"Value": ["sum", "mean", "median", "std"]})
 df.groupby(["Importers"]).agg({"Value": ["sum", "mean", "median", "std"]})
 
-catplot_df = df.groupby(["Exporter", "Importers"], as_index=False).agg({"Value": ["sum", "mean", "median", "std"]})
-cols = catplot_df.columns
-new_col = [i[0] if i[1] == '' else i[0] + '_' + i[1] for i in cols]
-catplot_df.columns = new_col
+catplot_df = df_group_agg(df, ["Exporter", "Importers"], {"Value": ["sum", "mean", "median", "std"]})
 
-# Turket +20000000$
-chart = sns.catplot(x='Importers', y='Value_sum', col='Exporter',
-                    data=catplot_df.loc[((catplot_df['Exporter'] == 'Turkey') & (catplot_df['Value_sum'] > 20000000) & (
-                            catplot_df['Importers'] != 'World'))], kind='bar')
-chart.set_xticklabels(rotation=90, horizontalalignment='right')
-plt.show()
+exporter = df['Exporter'].unique()
+sinir_Deger = [10000000, 200000000, 150000000, 500000, 15000000, 200000000]
 
-# USA +150000000$
-chart = sns.catplot(x='Importers', y='Value_sum', col='Exporter',
-                    data=catplot_df.loc[((catplot_df['Exporter'] == 'USA') & (catplot_df['Value_sum'] > 150000000))],
-                    kind='bar')
-chart.set_xticklabels(rotation=90, horizontalalignment='right')
-plt.show()
-
-# CHINA +200000000$
-chart = sns.catplot(x='Importers', y='Value_sum', col='Exporter',
-                    data=catplot_df.loc[((catplot_df['Exporter'] == 'China') & (catplot_df['Value_sum'] > 200000000))],
-                    kind='bar')
-chart.set_xticklabels(rotation=90, horizontalalignment='right')
-plt.show()
+for i, j in zip(exporter, sinir_Deger):
+    bar_plot(df_catplot=catplot_df, country=i, value=j, _x='Importers', _y='Value_sum', _col='Exporter')
 
 #######################
 # Elimizdeki tüm verilerin ticaret hacmi oranı
-colors = sns.color_palette('pastel')
-all_trade_exporter = df.groupby(["Exporter"]).sum()
-all_trade_exporter.plot(kind='pie', y='Value', x='Exporter', autopct='%1.2f%%',colors=colors,
-                        startangle=270, fontsize=17, shadow=True, explode=(0.1, 0.1, 0.1, 0.1, 0.1, 0.1), legend=False, title='Ticaret Hacimleri Oranı')
+pie_plot(_all_data=df, _x='Exporter', _y='Value', _title='Ticaret Hacimleri Oranı')
+############################################################################################
+############################################################################################
+############################################################################################
+# %%
+
+##################################################
+# Veri Seti export-imported-value
+##################################################
+all_data = pd.read_csv(r"VeriSetleri/All_Data/export-imported-value.csv", sep='\t')
+all_data.replace({'0': np.nan, 0: np.nan}, inplace=True)
+all_data = all_data.dropna(axis=0)
+col = all_data.columns
+cat_cols, cat_but_car, num_cols, num_but_cat = grab_col_names(all_data)
+check_df(all_data)
+
+# profile = pp.ProfileReport(all_data)
+# profile.to_file('profile.html')
+
+# Corr
+sns.heatmap(all_data.corr(), annot=True)
 plt.show()
 
-#######################
+# Bütün yılları tek kolona aldık
+df = all_data.melt(id_vars=["Exporters", 'Importer'],
+                   var_name="Year",
+                   value_name="Value")
 
+df.sort_values(by=['Exporters', 'Importer', 'Year'], axis=0, inplace=True)
+df = df.loc[df['Exporters'] != 'World']  # Bütün verilerin toplamı bir daha olduğu için atıyoruz
+df.reset_index(drop=True, inplace=True)
+
+# profile = pp.ProfileReport(df)
+# profile.to_file('profile2.html')
+
+check_df(df)
+
+# Ticaret value dağılımı nasıl ?
+df[['Value']].describe().T
+
+# Importer ülke sayısı ve ülkeler
+df[['Importer']].nunique()
+df['Importer'].unique()
+
+# Exporters ülke sayısı ve ülkeler
+df[['Exporters']].nunique()
+df['Exporters'].unique()
+
+# Exporters yılların yoplam-ortalama-medyan ve standart sapma değerleri
+df.groupby(["Exporters"]).agg({"Value": ["sum", "mean", "median", "std"]})
+df.groupby(["Importer"]).agg({"Value": ["sum", "mean", "median", "std"]})
+
+catplot_df = df_group_agg(df, ["Importer", "Exporters"], {"Value": ["sum", "mean", "median", "std"]})
+
+importer = df['Importer'].unique()
+sinir_Deger = [200000000, 150000000, 20000000, 210000000, 10000000, 500000]
+
+for i, j in zip(importer, sinir_Deger):
+    bar_plot(df_catplot=catplot_df, country=i, value=j, _x='Exporters', _y='Value_sum', _col='Importer')
+
+#######################
+# Elimizdeki tüm verilerin ticaret hacmi oranı
+pie_plot(_all_data=df, _x='Importer', _y='Value', _title='Ticaret Hacimleri Oranı')
+############################################################################################
+############################################################################################
+############################################################################################
+# %%
+##################################################
+# Veri Seti exporter-product
+##################################################
+all_data = pd.read_csv(r"VeriSetleri/All_Data/exporter-product.csv", sep='\t')
+all_data.replace({'0': np.nan, 0: np.nan}, inplace=True)
+all_data = all_data.dropna(axis=0)
+col = all_data.columns
+cat_cols, cat_but_car, num_cols, num_but_cat = grab_col_names(all_data)
+check_df(all_data)
+
+# profile = pp.ProfileReport(all_data)
+# profile.to_file('profile.html')
+
+# Corr
+sns.heatmap(all_data.corr(), annot=True)
+plt.show()
+
+# Bütün yılları tek kolona aldık
+df = all_data.melt(id_vars=["Exporter", 'Product label', 'Code'],
+                   var_name="Year",
+                   value_name="Value")
+
+df.sort_values(by=['Exporter', 'Product label', 'Code', 'Year'], axis=0, inplace=True)
+df = df.loc[df['Code'] != 'TOTAL']  # Bütün verilerin toplamı bir daha olduğu için atıyoruz
+df.reset_index(drop=True, inplace=True)
+
+# profile = pp.ProfileReport(df)
+# profile.to_file('profile2.html')
+
+check_df(df)
+
+# Ticaret value dağılımı nasıl ?
+df[['Value']].describe().T
+
+# Exporter ülke sayısı ve ülkeler
+df[['Exporter']].nunique()
+df['Exporter'].unique()
+
+# Ürün sayısı ve çeşitleri
+df[['Product label']].nunique()
+df['Product label'].unique()
+
+# Exporter ve ürünlerin   yoplam-ortalama-medyan ve standart sapma değerleri
+df.groupby(["Exporter"]).agg({"Value": ["sum", "mean", "median", "std"]})
+df.groupby(["Code"]).agg({"Value": ["sum", "mean", "median", "std"]})
+
+catplot_df = df_group_agg(df, ["Exporter", 'Product label'], {"Value": ["sum", "mean", "median", "std"]})
+
+exporter = df['Exporter'].unique()
+sinir_Deger = [10000000, 200000000, 150000000, 500000, 15000000, 150000000]
+
+for i, j in zip(exporter, sinir_Deger):
+    bar_plot(df_catplot=catplot_df, country=i, value=j, _x='Product label', _y='Value_sum', _col='Exporter')
+
+#######################
+# Elimizdeki tüm verilerin ticaret hacmi oranı
+pie_plot(_all_data=df, _x='Exporter', _y='Value', _title='Ticaret Hacimleri Oranı')
--- append_File.py
+++ append_File.py
@@ -2,54 +2,53 @@ import pandas as pd
 
 main_df = pd.DataFrame()
 
-arg = pd.read_csv('VeriSetleri\Argentina\Trade_Map_-_List_of_importing_markets_for_a_product_exported_by_Argentina.txt',
+arg = pd.read_csv('VeriSetleri\Argentina\Trade_Map_-_List_of_products_imported_by_Argentina.txt',
                   sep='\t')
 arg = arg.iloc[:, 0:-1]
 arg = arg.dropna(axis=0)
-arg['Exporter'] = 'Argentina'
+arg['Importer'] = 'Argentina'
 
-ch = pd.read_csv('VeriSetleri/China/Trade_Map_-_List_of_importing_markets_for_a_product_exported_by_China.txt',
+ch = pd.read_csv('VeriSetleri/China/Trade_Map_-_List_of_products_imported_by_China.txt',
                  sep='\t')
 ch = ch.iloc[:, 0:-1]
 ch = ch.dropna(axis=0)
 
-ch['Exporter'] = 'China'
+ch['Importer'] = 'China'
 
-ger = pd.read_csv('VeriSetleri/Germany/Trade_Map_-_List_of_importing_markets_for_a_product_exported_by_Germany.txt',
+ger = pd.read_csv('VeriSetleri/Germany/Trade_Map_-_List_of_products_imported_by_Germany.txt',
                   sep='\t')
 ger = ger.iloc[:, 0:-1]
 ger = ger.dropna(axis=0)
 
-ger['Exporter'] = 'Germany'
+ger['Importer'] = 'Germany'
 
-sen = pd.read_csv('VeriSetleri/Senegal/Trade_Map_-_List_of_importing_markets_for_a_product_exported_by_Senegal.txt',
+sen = pd.read_csv('VeriSetleri/Senegal/Trade_Map_-_List_of_products_imported_by_Senegal.txt',
                   sep='\t')
 sen = sen.iloc[:, 0:-1]
 sen = sen.dropna(axis=0)
 
-sen['Exporter'] = 'Senegal'
+sen['Importer'] = 'Senegal'
 
-tur = pd.read_csv('VeriSetleri/Turkey/Trade_Map_-_List_of_importing_markets_for_a_product_exported_by_Turkey.txt',
+tur = pd.read_csv('VeriSetleri/Turkey/Trade_Map_-_List_of_products_imported_by_Turkey.txt',
                   sep='\t')
 tur = tur.iloc[:, 0:-1]
 tur = tur.dropna(axis=0)
 
-tur['Exporter'] = 'Turkey'
+tur['Importer'] = 'Turkey'
 
 usa = pd.read_csv(
-    'VeriSetleri/USA/Trade_Map_-_List_of_importing_markets_for_a_product_exported_by_United_States_of_America.txt',
+    'VeriSetleri/USA/Trade_Map_-_List_of_products_imported_by_United_States_of_America.txt',
     sep='\t')
 usa = usa.iloc[:, 0:-1]
 usa = usa.dropna(axis=0)
 
-usa['Exporter'] = 'USA'
+usa['Importer'] = 'USA'
 
 frames = [arg, ch, ger, sen, tur, usa]
 
 main_df = pd.concat(frames)
 
-main_df.to_csv('import-exported-value.csv', sep='\t', index=False)
+main_df.to_csv('importer-product.csv', sep='\t', index=False)
 
 
 del main_df
-
